62c15364729854bf3df89503b8495c8b0773d7a3
deeivihh <deeivihh@hotmail.com>
2026-07-26 18:45:51 +0200

    Add `types.ts`

---
+++ src/app/utils/types.ts
@@ -0,0 +1,21 @@
+export interface Site {
+    municipio: string;
+    cod_municipio: string;
+    provincia: string;
+    cod_provincia: string;
+    cod_ine: number;
+    poblacion: number;
+    mancomunidades: string | null;
+    entidades_locales_menores: string | null;
+    comarca: string | null;
+    longitud: number;
+    latitud: number;
+    coordenadax: number;
+    coordenaday: number;
+    posicion: {
+        lon: number;
+        lat: number;
+    };
+    presencia_de_comercio: string;
+    distance?: number;
+}
