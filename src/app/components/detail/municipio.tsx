39f6635e05e7f08b1b177a7d24948d034528ea90
deeivihh <deeivihh@hotmail.com>
2026-08-10 17:14:55 +0200

    Minors fixes

--- src/app/components/detail/municipio.tsx
+++ src/app/components/detail/municipio.tsx
@@ -80,7 +80,7 @@ export default function MunicipioDetail({ cod }: { cod: string }) {
     if (!data) return <div>Municipio no encontrado.</div>;
 
     return (
-        <div className="flex flex-col gap-2 justify-center items-center min-md:mx-auto w-full h-full max-w-6xl">
+        <div className="flex flex-col gap-2 justify-center items-center min-md:mx-auto w-full h-full">
             <div className="flex flex-col bg-bg-card rounded-[32px] my-4 pb-8 w-full min-h-[94svh] overflow-hidden border-3 border-title/10">
                 <section className="flex flex-col justify-end items-start overflow-hidden relative w-full h-fit min-h-[20svh]">
                     <div className="flex flex-col gap-4 max-md:py-6 p-8 pb-6 h-full w-full">
--- src/app/municipio/[cod]/page.tsx
+++ src/app/municipio/[cod]/page.tsx
@@ -1,4 +1,5 @@
 import MunicipioDetail from "@/app/components/detail/municipio";
+import SearchBar from "@/app/components/home/searchbar";
 
 export default async function DetailPage({
     params,
@@ -8,8 +9,14 @@ export default async function DetailPage({
     const { cod } = await params;
 
     return (
-        <main className="flex flex-col justify-start items-center min-h-screen w-full max-md:p-4 p-4">
-            <MunicipioDetail cod={cod} />
+        <main className="flex flex-col justify-center mx-auto items-center min-h-screen w-full max-md:p-4 p-4 max-w-6xl">
+            <section className="w-full">
+                <SearchBar />
+            </section>
+
+            <section className="flex w-full h-full mt-2">
+                <MunicipioDetail cod={cod} />
+            </section>
         </main>
     );
 }
