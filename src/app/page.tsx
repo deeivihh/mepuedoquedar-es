925b29779d3c95d80687565347e8536ad875871c
deeivihh <deeivihh@hotmail.com>
2026-08-10 17:51:10 +0200

    Responsive fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,28 +8,26 @@ export default function Home() {
     return (
         <main className="flex flex-col justify-start h-screen w-full max-md:p-6">
             <footer className="absolute left-1/2 -translate-x-1/2 bottom-4 max-lg:top-8">
-                <div className="flex gap-2 items-start justify-center">
+                <div className="flex max-[18rem]:flex-col gap-6 max-md:gap-4 items-start justify-center">
                     <Link
                         href="/metodologia"
-                        className="text-title/75 hover:text-title leading-relaxed text-sm flex items-center justify-center gap-2"
+                        className="text-title/75 hover:text-title leading-relaxed text-sm max-[21rem]:text-xs flex items-center justify-center gap-2"
                     >
                         <FaProjectDiagram />
                         Metodología
                     </Link>
-                    ·
                     <Link
                         href="https://github.com/deeivihh/mepuedoquedar"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-title/75 hover:text-title leading-relaxed text-sm mb-4 flex items-center justify-center gap-2"
+                        className="text-title/75 hover:text-title leading-relaxed text-sm max-[21rem]:text-xs flex items-center justify-center gap-2"
                     >
                         <FaGithub />
                         Repositorio
                     </Link>
-                    ·
                     <Link
                         href="mailto:deeivihh3@hotmail.com"
-                        className="text-title/75 hover:text-title leading-relaxed text-sm mb-4 flex items-center justify-center gap-2"
+                        className="text-title/75 hover:text-title leading-relaxed text-sm max-[21rem]:text-xs flex items-center justify-center gap-2"
                     >
                         <MdEmail />
                         Contacto
