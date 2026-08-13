1015068bdebf07c8371a9c1dcfe97d4dcb4f3aab
deeivihh <deeivihh@hotmail.com>
2026-08-13 16:06:42 +0200

    Add `og` stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Source_Serif_4, Inter } from "next/font/google"
 import "./globals.css";
 import { PreferencesProvider } from "@/app/contexts/PreferencesContext";
@@ -13,14 +12,6 @@ const textFont = Inter({
 	variable: "--font-text",
 })
 
-export const metadata: Metadata = {
-	title: "¿Me puedo quedar?",
-	description: "",
-	icons: {
-		icon: "/favicon.ico",
-	},
-};
-
 export default function RootLayout({
 	children,
 }: Readonly<{
@@ -28,6 +19,38 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="es">
+			<head>
+				<meta property="og:site_name" content="¿Me puedo quedar?" />
+				<meta name="description" content="Descubre dónde vivir en Castilla y León. Encuentra municipios con oportunidades, servicios y calidad de vida para construir tu próximo hogar." />
+				<meta property="og:description" content="¿Y si tu próximo hogar estuviera en Castilla y León? Encuentra el municipio donde sí te puedes quedar." />
+				<meta property="og:image" content="https://mepuedoquedar.es/og/og-image.jpg" />
+				<meta property="og:url" content="https://mepuedoquedar.es" />
+				<meta property="og:locale" content="es_ES" />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@deeivihh" />
+				<meta name="theme-color" content="#E7DCC3" />
+				<link rel="canonical" href="https://mepuedoquedar.es" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+				<link rel="icon" type="image/x-icon" sizes="64x64" href="/favicon/favicon.ico" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+				<link rel="manifest" href="/favicon/site.webmanifest" />
+				<script
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{
+						__html: JSON.stringify({
+							"@context": "https://schema.org",
+							"@type": "WebSite",
+							"@id": "https://mepuedoquedar.es/#website",
+							url: "https://mepuedoquedar.es/",
+							name: "¿Me puedo quedar?",
+							description:
+								"Descubre dónde vivir en Castilla y León. Encuentra municipios con oportunidades, servicios y calidad de vida para construir tu próximo hogar.",
+							inLanguage: "es-ES"
+						})
+					}}
+				/>
+			</head>
 			<body className={`${titleFont.variable} ${textFont.variable} antialiased`}>
 				<PreferencesProvider>
 					{children}
