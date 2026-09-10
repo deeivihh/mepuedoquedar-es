87923e2d7327c04ac265fb20f4506ce43231e55b
deeivihh <deeivihh@hotmail.com>
2026-09-10 19:06:27 +0200

    Add cache-control headers for municipio routes in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -116,6 +116,15 @@ const nextConfig: NextConfig = {
 							value: "public, max-age=0, s-maxage=86400, stale-while-revalidate=604800",
 						},
 					],
+				},
+				{
+					source: "/municipio/:path*",
+					headers: [
+						{
+							key: "Cache-Control",
+							value: "public, max-age=0, s-maxage=86400, stale-while-revalidate=604800",
+						},
+					],
 				}
 			);
 		}
